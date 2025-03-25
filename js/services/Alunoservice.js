8d012a2177a1943421686726f19e5a481ba3ece9
Guilherme-1605 <guilhermepenna155@gmail.com>
2025-03-25 14:52:45 -0300

    view

---
+++ js/services/Alunoservice.js
@@ -0,0 +1,20 @@
+class AlunoService {
+    constructor() {
+        this.alunos =[]
+    }
+
+    add(aluno){
+        if(!aluno instanceof AlunoModel){
+            throw new Error('O aluno deve ser uma instância de AlunoModel')
+        }
+        this.alunos.push(aluno)
+    }
+
+    update(aluno) {
+        return aluno
+    }
+
+    searchById(id) {
+        return this.alunos.find(aluno => aluno._id === id)
+    }
+}
