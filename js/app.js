d9b2da9912dc15246b73958ea49417e6995bade8
Guilherme-1605 <guilhermepenna155@gmail.com>
2025-03-31 15:14:29 -0300

    Usando LocalStorage

--- js/app.js
+++ js/app.js
@@ -1,44 +1,10 @@
-const alunos = [
-  {
-    nome: "chico melato",
-    notas: {
-      backend_1: [1, 1, 2, 2],
-      frontend_2: [2, 2, 2, 2],
-      bancodados: [2, 2, 3, 3],
-      ferramentas: [3, 3, 3, 3],
-    },
-  },
-  {
-    nome: "talita lima",
-    notas: {
-      backend_1: [4, 4, 4, 4],
-      frontend_2: [4, 4, 5, 5],
-      bancodados: [5, 9, 6, 6],
-      ferramentas: [7, 7, 8, 9],
-    },
-  },
-  {
-    nome: "Fulano da Silva",
-    notas: {
-      backend_1: [9, 9, 9, 9],
-      frontend_2: [9, 9, 9, 9],
-      bancodados: [9, 9, 9, 9],
-      ferramentas: [9, 9, 9, 9],
-    },
-  },
-];
-
 const alunoService = new AlunoService()
-
-alunos.forEach(aluno => {
-  alunoService.add(new AlunoModel(aluno))
-});
 
 const alunoView = new AlunoView(document.querySelector('[data-table-alunos]'))
 const alunoController = new AlunoController(alunoService, alunoView)
 
 document.querySelector('form').addEventListener('submit', function (event) {
   event.preventDefault()
-  const nome = document.getElementById('first_name').Value
+  const nome = document.getElementById('first_name').value
   alunoController.add({ nome })
 })
